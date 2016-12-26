7fa792706602df7885b44368100e1d11acd432c5
Rhnbmpl <rb13@iitbbs.ac.in>
2016-12-26 15:06:17 +0530

    Update

---
+++ auto.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+import os
+import subprocess,time,datetime
+end=''
+start_url=''
+cwd=os.path.dirname(os.path.realpath(__file__))#get path to the directory where the program is in
+tym=datetime.datetime.now().strftime('%H:%M:%S %d-%B-%Y')
+print("---------LOG AT: %s --------"%(tym))# all print statements and error recorded in prog.log file
+if(os.path.exists('%s/dont_touch.config'%(cwd))==False):
+	cwd=os.getcwd()
+	conf=open('%s/dont_touch.config'%(cwd),'w')#w=only write;r=only read;r+=read and write
+	conf.close()
+	end='#'
+	print('Setting up crontab entry for updating comics at 10am and 2 pm everyday.....')
+	subprocess.run('crontab -l > mycron',shell=True)
+	subprocess.run('echo "00 10,14 * * * %s/auto.py > %s/prog.log 2>&1 " >> mycron'%(cwd,cwd),shell=True)
+	subprocess.run('crontab mycron',shell=True)
+	subprocess.run('rm mycron',shell=True)
+	print('Do you want to update comics on startup?[y/n]')
+	if(str(input())=='y'):
+		subprocess.run('crontab -l > mycron',shell=True)
+		subprocess.run('echo "@reboot %s/auto.py > %s/prog.log 2>&1 " >> mycron'%(cwd,cwd),shell=True)
+		subprocess.run('crontab mycron',shell=True)
+		subprocess.run('rm mycron',shell=True)
+	else:
+		pass
+else:
+	conf=open('%s/dont_touch.config'%(cwd),'r')
+	line=(conf.readline()).split()
+	end=line[0]
+	start_url=line[1]
+	conf.close()
+
+if(os.path.exists('xkcd_multi')==False):
+	os.makedirs('xkcd_multi',exist_ok=True)
+
+import xkcd_down_multi as downl
+end,start_url=downl.main(end,start_url)
+if(end=='nope'):
+	print("----Already updated to latest-----")
+
+else:
+	conf=open('%s/dont_touch.config'%(cwd),'w')#w=only write;r=only read;r+=read and write
+	conf.write(end+' '+start_url+' '+tym)
+	conf.close()
+	print("----Updated till: %s ----- On Last run: %s -----"%(end,tym))
+subprocess.run('rm %s/xkcd.txt'%(cwd),shell=True)
+#print(cwd)
+
+
+
+
+# crontab format: min hr date-of-mnth mnth day-of-wk [command]
--- xkcd_down_multi.py
+++ xkcd_down_multi.py
@@ -1,69 +1,88 @@
 #!/usr/bin/python3
 import webbrowser as web
-import requests,bs4,os,sys,threading,time
-addr='http://www.xkcd.com'
-#end=list(sys.argv)
-prev_addr=addr
-prev_url=''
-error=[]
-end='#'
-if(os.path.exists('xkcd_multi')==False):
-	os.makedirs('xkcd_multi',exist_ok=True)
-def down(down_url,prev_addr):
+import requests,bs4,os,sys,threading,time,subprocess
+def down(down_url,prev_addr,cwd):
 	try:
 			res=requests.get('http:'+down_url)
 			print('Downloading Image......%s'%('http:'+down_url))
-			imgfile=open('xkcd_multi/'+os.path.basename(down_url),'wb')
+			imgfile=open('%s/xkcd_multi/'%(cwd)+os.path.basename(down_url),'wb')
 			for chunk in res.iter_content(100000):	#write in chunks of 100000 to prevent memory leaks
 				imgfile.write(chunk)
 			imgfile.close()
 	except:
 			print('Error downloading image : %s'%(prev_addr))
 			pass
-downthr_lst=[]
-while 1:
-	s=requests.Session()
-	pg=s.get(prev_addr)
-	try:
-		pg.raise_for_status()
-	except Exception as exc:
-		if(pg.status_code==504):
-			print('Error fetching page: %s'%(exc))
-			print('Skipping comic : %s'%(prev_addr))
+def main(end,start_url):
+	cwd=os.path.dirname(os.path.realpath(__file__))#get path to the directory where the program is in
+	fst=0
+	ret=['','']
+	downthr_lst=[]
+	addr='http://www.xkcd.com'
+	#end=list(sys.argv)
+	prev_addr=addr
+	prev_url=''
+	error=[]
+	while 1:
+		s=requests.Session()
+		pg=s.get(prev_addr)
+		try:
+			pg.raise_for_status()
+		except Exception as exc:
+			if(pg.status_code==504):
+				print('Error fetching page: %s'%(exc))
+				print('Skipping comic : %s'%(prev_addr))
+				error.append(prev_addr)
+				prev_url=int(prev_url.replace('/',''))
+				prev_url=prev_url-1
+				prev_url=str(prev_url)
+				prev_url='/'+prev_url+'/'
+				prev_addr=addr+prev_url
+				continue
+			else:
+				print('Error feteching page : %s'%(exc))
+				error.append(prev_addr)
+		file=open('%s/xkcd.txt'%(cwd),'wb')
+		for chunk in pg.iter_content(100000):
+			file.write(chunk)
+		soup=bs4.BeautifulSoup(pg.text,'lxml')
+		img=soup.select('#comic img')
+		if img==[]:
+			print('--------Could not find image : %s-------'%(prev_addr))
 			error.append(prev_addr)
-			prev_url=int(prev_url.replace('/',''))
-			prev_url=prev_url-1
-			prev_url=str(prev_url)
-			prev_url='/'+prev_url+'/'
-			prev_addr=addr+prev_url
-			continue
 		else:
-			print('Error feteching page : %s'%(exc))
-			error.append(prev_addr)
-	file=open('xkcd.txt','wb')
-	for chunk in pg.iter_content(100000):
-		file.write(chunk)
-	soup=bs4.BeautifulSoup(pg.text,'lxml')
-	img=soup.select('#comic img')
-	if img==[]:
-		print('--------Could not find image : %s-------'%(prev_addr))
-		error.append(prev_addr)
-	else:
-		down_url=img[0].get('src')
-		down_thr=threading.Thread(target=down,args=(down_url,prev_addr))
-		downthr_lst.append(down_thr)
-		down_thr.start()
-		
-	file.close()
-	prev=soup.select('a[rel="prev"]')
-	prev_url=prev[0].get('href')
-	if (prev_url==end):
-		break
-	else:
-		prev_addr=addr+prev_url
-for down_thr in downthr_lst:
-	down_thr.join()
-print('-----------Errors-----------')
-for k in range(0,len(error)):
-	print(error[k])
-print('------------Done------------')
+			down_url=img[0].get('src')
+
+			if(down_url==start_url and fst==0):
+				print(start_url+" ....Exists, no new comics :'( ....")
+				ret=['nope','nope']
+				return ret
+			elif(fst==0):
+				ret[1]=down_url
+			else:
+				pass
+
+			down_thr=threading.Thread(target=down,args=(down_url,prev_addr,cwd))
+			downthr_lst.append(down_thr)
+			down_thr.start()
+			
+		file.close()
+		prev=soup.select('a[rel="prev"]')
+		prev_url=prev[0].get('href')
+		if(fst==0):
+			start=prev_url
+			start=int(start.replace('/',''))
+			start=str(start+1)
+			ret[0]='/'+start+'/'
+			fst=1
+
+		if (prev_url==end):
+			break
+		else:
+			prev_addr=addr+prev_url
+	for down_thr in downthr_lst:
+		down_thr.join()
+	print('-----------Errors: %d -----------'%(len(error)))
+	for k in range(0,len(error)):
+		print(error[k])
+	print('--------------Done--------------')
+	return ret
